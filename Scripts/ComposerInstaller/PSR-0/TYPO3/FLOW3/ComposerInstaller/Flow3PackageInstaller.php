271e9047597643112bd24edc35769f237f2952e2
Christian Jul Jensen <julle@typo3.org>
2012-08-01 12:12:05 +0200

    [TASK] Integrate ComposerInstaller
    Add Installer class for Composer, and composer manifest for package
    
    Note that the installer most be located in a PSR-0 compatible path to work.
    
    Change-Id: I2dc488c5e9151cc53f221ff794baaa144d0877c1
    Releases: 1.2
    
    [TASK] Move ComposerInstaller to Scripts directory
    
    Since composer requires PSR-0 path and because the installer class
    cannot be reflected it should be move from Classes to Scripts
    
    Change-Id: I2dc488c5e9151cc53f221ff794baaa144d0877c1
    releases: 1.2

---
+++ Scripts/ComposerInstaller/PSR-0/TYPO3/FLOW3/ComposerInstaller/Flow3PackageInstaller.php
@@ -0,0 +1,50 @@
+<?php
+namespace TYPO3\FLOW3\ComposerInstaller;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 framework.                            *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ * of the License, or (at your option) any later version.                 *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use Composer\Package\PackageInterface;
+
+/**
+ * FLOW3 Installer-class for Composer
+ *
+ */
+class Flow3PackageInstaller extends \Composer\Installer\LibraryInstaller {
+
+	/**
+	 * Check whether the package type is supported by this installer
+	 *
+	 * @param string $packageType
+	 * @return bool
+	 */
+	public function supports($packageType) {
+		return ($packageType === 'library' || 0 === strstr($packageType,'flow3'))?TRUE:FALSE;
+	}
+
+	/**
+	 * Create the FLOW3 compatible install path
+	 *
+	 * @param \Composer\Package\PackageInterface $package
+	 * @return string
+	 */
+	public function getInstallPath(PackageInterface $package) {
+		$autoloadDefinition = $package->getAutoload();
+		if(isset($autoloadDefinition['psr-0'])) {
+			$namespace = key($autoloadDefinition['psr-0']);
+		}
+		$FLOW3StylePackageKey = str_replace('\\','.',$namespace);
+		if($FLOW3StylePackageKey === 'Composer') {
+			//Composer will not work unless it has 2 levels to the config files
+			$FLOW3StylePackageKey = 'composer/composer';
+		}
+		return $this->vendorDir . '/' . $FLOW3StylePackageKey;
+	}
+}
