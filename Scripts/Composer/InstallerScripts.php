0a5b8cb9940ddd3ff549fddf717a9dc4105f5883
Christian Jul Jensen <julle@typo3.org>
2012-09-04 15:59:15 +0200

    [TASK] Fix spelling errors

--- Scripts/Composer/InstallerScripts.php
+++ Scripts/Composer/InstallerScripts.php
@@ -1,9 +1,9 @@
 <?php
-namespace TYPO3\FLOW3\Composer\;
+namespace TYPO3\FLOW3\Composer;
 
 use Composer\Script\Event;
 
-class IntallerScripts
+class InstallerScripts
 {
 	static public function postUpdateAndInstall(Event $event) {
 		var_dump($event);
